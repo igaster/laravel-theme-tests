8e1ec4c6a9f1af706976f99149a6654d76fd91f1
Giannis Gasteratos <igasteratos@gmail.com>
2019-09-05 11:17:53 +0300

    update packages

--- config/session.php
+++ config/session.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
 
     /*
@@ -124,8 +126,9 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        str_slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
     ),
+
 
     /*
     |--------------------------------------------------------------------------
